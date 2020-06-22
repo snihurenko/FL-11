6763f83a2006597a1faaca3f83869721842fce18
Anastasiia Snihurenko <snigurenko.anastasiya@gmail.com>
2020-06-22 17:03:18 +0300

    Add files via upload

---
+++ buttlership_game/main.js
@@ -0,0 +1,117 @@
+//view object is responsible for updating the view.
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+
+    displayMiss: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+};
+
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMessage("Tap tap, is this thing on?");
+
+//model is where we keep the state of the game.
+
+var model = {
+    boardSize: 7, //size of the game grid
+    numShips: 3, //ships in the game
+    shipLength: 3, //number of locations in each ship
+    shipsSunk: 0,
+
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+            
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            locations = ship.locations;
+            var index = locations.indexOf(guess);   //check if there is a match
+
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+
+        return false;
+    },
+
+    isSunk: function(ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// model.fire("53");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+
+
+var controller = {
+    guesses: 0,
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location){
+            this.guesses++;
+            var hit = model.fire(location);
+
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " +
+                this.guesses + " guesses");
+            }
+        }
+    }
+};
+
+//get and process user input
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.charAt(1);
+    
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize ||
+            column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
